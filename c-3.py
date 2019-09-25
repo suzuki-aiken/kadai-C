99363f4427836f0b7588ea3a2865dfa86e5dc79c
suzuki-ai <karikakko10@gmail.com>
2019-09-25 16:52:42 +0900

    伏字の入力ができるように修正

--- c-3.py
+++ c-3.py
@@ -2,16 +2,20 @@
 C-3: <censored> を自由に変更できるようにする
 NGワードが含まれていた場合に置き換える文字列を自由に設定できるようにしてください
 """
+
+
 class WordFilter:
-    def __init__(self, object):
+    def __init__(self, object, 伏字):
         self.NG = object
+        self.hide = 伏字
 
     def censor(self, content):
         print(content.replace(self.NG, '<censored>'))
 
 
 def main():
-    my_filter = WordFilter("アーセナル")
+    hide_word=input("伏字を入力してください：")
+    my_filter = WordFilter("アーセナル",hide_word)
 
     # "昨日の<？？？？？>の試合アツかった！"
     my_filter.censor("昨日のアーセナルの試合アツかった！")
