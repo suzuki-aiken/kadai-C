83b42ecd2d8c1616c2630e579bd9680498d1c9d5
suzuki-ai <karikakko10@gmail.com>
2019-09-26 09:11:07 +0900

    C-5 課題　NGワードを複数登録できるようにする
    変更点
    NGワードの別のNGリスト２を用意　c-3.pyを参考に追加
    
    問題点
    NGワードがNGリスト内の用語に書き換わってしまった

--- c-5.py
+++ c-5.py
@@ -5,19 +5,23 @@ NGワードを後から変更できるようにしてください
 
 
 class WordFilter:
-    def __init__(self, object):
+    def __init__(self, object, 伏字):
         self.NG = object
+        self.hide = 伏字
 
     def censor(self, content):
         for i in self.NG:
-            content = content.replace(i, '<censored>')
+            for j in self.hide:
+                content = content.replace(i, j)
 
         return print(f'{content}')
 
 
 def main():
     NG_list = ["アーセナル", "アツ", "bbb"]
-    my_filter = WordFilter(NG_list)
+    NG_list2 = ["アーセナル", "試合", "bbb"]
+
+    my_filter = WordFilter(NG_list, NG_list2)
 
     my_filter.censor("昨日のアーセナルの試合アツかった！")
 
